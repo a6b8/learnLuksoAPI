f701399f08b28f4b7ad17be181d59d3c083cf660
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2023-11-23 18:15:36 +0100

    update routes

--- src/LearnLuksoServer.mjs
+++ src/LearnLuksoServer.mjs
@@ -1,6 +1,8 @@
 import { config } from './data/config.mjs'
 import { printMessages } from './helpers/mixed.mjs'
 import { LSPInteraction } from './services/LSPInteraction.mjs'
+import { fetch } from 'node-fetch'
+
 
 import express from 'express'
 import fs from 'fs'
